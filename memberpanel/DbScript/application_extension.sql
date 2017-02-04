f279e40b88cd6f52a5fcbbcb8f58a9ca114025b9
websnipe-abhik <abhik@websnipeit.com>
2017-02-04 13:10:00 +0530

    Commit for SQLScript
    Commit for SQLScript

---
+++ memberpanel/DbScript/application_extension.sql
@@ -0,0 +1,43 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.6.11 : Database - mantrahe_data
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mantrahe_data` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `mantrahe_data`;
+
+/*Table structure for table `application_extension` */
+
+DROP TABLE IF EXISTS `application_extension`;
+
+CREATE TABLE `application_extension` (
+  `tran_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_of_application` date DEFAULT NULL,
+  `member_id` int(11) DEFAULT NULL,
+  `membership_no` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
+  `validity_period` varchar(50) COLLATE latin1_general_ci DEFAULT NULL,
+  `from_date` date DEFAULT NULL,
+  `to_date` date DEFAULT NULL,
+  `grant_from` date DEFAULT NULL,
+  `grant_to` date DEFAULT NULL,
+  `grant_days` int(11) DEFAULT NULL,
+  `branch_code` varchar(20) COLLATE latin1_general_ci DEFAULT NULL,
+  `card_code` varchar(10) COLLATE latin1_general_ci DEFAULT NULL,
+  `company_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`tran_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=362 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;

2250d1fa004193fdf0a15bc94b315378911bcfb7
websnipe-abhik <abhik@websnipeit.com>
2017-02-10 17:53:18 +0530

    Delete on BC
    Delete on BC

--- memberpanel/application/models/profilemodel.php
+++ memberpanel/application/models/profilemodel.php
@@ -455,7 +455,10 @@ class profilemodel extends CI_Model {
                     "waist_hip_point"=>$row->waist_hip_point,
                     "waist_hip_remarks"=>$row->waist_hip_remarks,
                     "image_name"=>$row->image_name,
-                    "date_of_entry"=>$row->date_of_entry
+                    "date_of_entry"=>$row->date_of_entry,
+					"image_width"=>$this->getImageWidth($row->image_name),
+					"image_height"=>$this->getImageHeight($row->image_name)
+					
                 );
                }
                return $data;
@@ -465,6 +468,53 @@ class profilemodel extends CI_Model {
         return $data;
         }
     }
+	
+	private function getImageWidth($imagename){
+		//realpath(APPPATH.'../assets/img');
+		$image_width=0;
+		$imagepath = APPPATH . 'assets/images/portfolioimages/'.$imagename;
+		if($imagename!=""){
+			if(file_exists($imagepath)){
+				list($width, $height) = getimagesize($imagepath);
+				$image_width = $width;
+				return $image_width;
+			}
+			else{
+				return $image_width;
+			}
+			
+		}
+		else{
+			return $image_width;
+		}
+		
+		
+		
+		
+	}
+	
+	private function getImageHeight($imagename){
+		$image_height =0;
+		$imagepath = realpath(APPPATH . 'assets/images/portfolioimages/'.$imagename);
+		
+		if($imagename!=""){
+			if(file_exists($imagepath)){
+				list($width, $height) = getimagesize($imagepath);
+				$image_height = $height;
+				return $image_height;
+			}
+			else{
+				return $image_height;
+			}
+			 
+		}
+		else{
+			 return $image_height;
+		}
+		
+		
+	}
+
     
     public function getBodyCompositionImageById($id){
         $image="";
